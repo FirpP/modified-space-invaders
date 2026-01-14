5f9a6cdad785a850bae78ab6a259defdc0154d7c
FirpP <firp@superbox.pl>
2026-01-14 09:06:24 +0100

    do dokumentacji w doxygen

---
+++ docs/html/ConsoleUtils_8cpp.js
@@ -0,0 +1,8 @@
+var ConsoleUtils_8cpp =
+[
+    [ "clearScreen", "ConsoleUtils_8cpp.html#a9d7e8af417b6d543da691e9c0e2f6f9f", null ],
+    [ "getKey", "ConsoleUtils_8cpp.html#a2da36c055d23bb1f20f20bfabbb40661", null ],
+    [ "isKeyPressed", "ConsoleUtils_8cpp.html#a0ab7dad392b17984909e6227968313e2", null ],
+    [ "kbhit", "ConsoleUtils_8cpp.html#a97e9b1fe8d4c010474637a654aad6566", null ],
+    [ "setupConsole", "ConsoleUtils_8cpp.html#a8196ab970ce52abe1854d7474f260534", null ]
+];
---
+++ docs/html/search/functions_f.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['_7eentity_0',['~Entity',['../classEntity.html#adf6d3f7cb1b2ba029b6b048a395cc8ae',1,'Entity']]],
+  ['_7egame_1',['~Game',['../classGame.html#ae3d112ca6e0e55150d2fdbc704474530',1,'Game']]]
+];
